1e56afd41b93418d35b0762404bbfe670dc5177d
Léo Wadin <wadinleo@gmail.com>
2024-12-06 00:14:07 +0100

    feat(components): add answer to quizz component

--- src/app/Components/quizz/quizz.component.ts
+++ src/app/Components/quizz/quizz.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import {NgForOf, NgIf} from '@angular/common';
+import {NgClass, NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-quizz',
   standalone: true,
   imports: [
     NgIf,
-    NgForOf
+    NgForOf,
+    NgClass
   ],
   templateUrl: './quizz.component.html',
   styleUrl: './quizz.component.scss'
@@ -16,14 +17,30 @@ export class QuizzComponent {
   totalQuestions = 3;
   userAnswer: string = '';
 
+  isAnswered = false;
+  feedbackMessage = '';
+
   question = {
     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
-    type: 'text', // Peut être 'truefalse', 'multiple', ou 'text'
-    options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'], // Utilisé uniquement pour 'multiple'
+    type: 'truefalse', // Peut être 'truefalse', 'multiple', ou 'text'
+    options: ['Option 1', 'Option 2', 'Option 3'], // Utilisé uniquement pour 'multiple'
+    correctAnswer: true // La réponse correcte (ou un tableau pour 'multiple')
   };
 
+  isCorrect = false; // Définit si la réponse est correcte
+
   answer(response: any): void {
-    console.log('Réponse utilisateur :', response);
-    // Ajoutez ici la logique pour gérer la réponse
+    if (this.question.type === 'text') {
+      this.isCorrect = response.trim().toLowerCase() === this.question.correctAnswer.toString().trim().toLowerCase();
+    } else {
+      this.isCorrect = response === this.question.correctAnswer;
+    }
+
+    // Met à jour l'état et le message
+    this.isAnswered = true;
+    this.feedbackMessage = this.isCorrect ? 'Bonne réponse !' : 'Mauvaise réponse !';
+
+    console.log('Réponse utilisateur :', response, ' | Correcte :', this.isCorrect);
   }
+
 }
