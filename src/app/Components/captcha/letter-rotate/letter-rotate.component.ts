3aeeae0e717c7bf37d1b62945b68e0243027874e
Matheo H-M <matheo3859@gmail.com>
2024-12-06 00:31:00 +0100

    build : generate rotate captcha

---
+++ src/app/Components/captcha/letter-rotate/letter-rotate.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-letter-rotate',
+  standalone: true,
+  imports: [],
+  templateUrl: './letter-rotate.component.html',
+  styleUrl: './letter-rotate.component.scss'
+})
+export class LetterRotateComponent {
+  urlToAsset: string = "../../../../assets/captcha/";
+  rotationAngle!: number;
+  isTransitionEnabled: boolean = true;
+
+  ngOnInit(){
+    this.rotationAngle = 0;
+    this.urlToAsset = this.urlToAsset + "T.png";
+    this.rotateImage(this.getRandomInt(1, 8));
+  }
+
+  getRandomInt(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  rotateImage(nbrRotation: number = 1): void {
+    if (this.rotationAngle + 45 == 360 && nbrRotation == 1) {
+      this.rotationAngle += 45;
+      setTimeout(() => {
+        this.isTransitionEnabled = false; // Désactive la transition
+        this.rotationAngle = 0;
+      }, 600);
+      
+      setTimeout(() => {
+        this.isTransitionEnabled = true; // Réactive la transition après un délai
+      }, 650); 
+    }
+    else{
+      this.rotationAngle += 45*nbrRotation; // Rotation de 45 degrés à chaque clic
+      this.rotationAngle %= 360; // Maintenir l'angle entre 0 et 360
+    }
+  }
+}
