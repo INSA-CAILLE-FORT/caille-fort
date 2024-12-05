50bf0f8110a85990a8707e2fb6d28d8fbf16132e
Elena <elenabeylat@gmail.com>
2024-12-05 19:42:04 +0100

    feat : header before style

--- src/app/Pages/about/about.component.ts
+++ src/app/Pages/about/about.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent} from '../../Components/header/header.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [
+    HeaderComponent,
+  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
