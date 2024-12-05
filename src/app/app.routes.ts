5576050ef3dafa9e7c513a9fb957f81168058291
Matheo H-M <matheo3859@gmail.com>
2024-12-06 00:22:12 +0100

    refactor : add route to captcha

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,12 @@ import { MapComponent } from './Pages/map/map.component';
 import { GameComponent} from './Pages/game/game.component';
 import { PodcastComponent} from './Pages/podcast/podcast.component';
 import { AboutComponent} from './Pages/about/about.component';
+import { CaptchaComponent } from './Pages/captcha/captcha.component';
 
 export const routes: Routes = [
   { path: '', component: WelcomeComponent },
   { path: 'map', component: MapComponent },
   { path: 'game/:id', component: GameComponent },
   { path: 'podcast', component: PodcastComponent },
-  { path: 'about', component: AboutComponent },
-  { path: '**', redirectTo: '/map' }
+  { path: 'captcha', component: CaptchaComponent }
 ];
