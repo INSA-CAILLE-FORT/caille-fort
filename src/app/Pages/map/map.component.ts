e527339b00b5c215af386fd537b73f6cc3ca15af
Léo Wadin <wadinleo@gmail.com>
2024-12-06 03:14:04 +0100

    feat(map): definitive points placement method

--- src/app/Pages/map/map.component.ts
+++ src/app/Pages/map/map.component.ts
@@ -1,8 +1,9 @@
-import {Component, HostListener} from '@angular/core';
+/* src/app/Pages/map/map.component.ts */
+import { Component, HostListener } from '@angular/core';
 import { HeaderComponent } from '../../Components/header/header.component';
-import {QuizzComponent} from '../../Components/quizz/quizz.component';
-import {TileComponent} from '../../Components/tile/tile.component';
-import {NgForOf, NgStyle} from '@angular/common';
+import { QuizzComponent } from '../../Components/quizz/quizz.component';
+import { TileComponent } from '../../Components/tile/tile.component';
+import { NgForOf, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-map',
@@ -20,22 +21,18 @@ import {NgForOf, NgStyle} from '@angular/common';
 export class MapComponent {
   points = [
     { x: 23, y: 48 },
-    { x: 33, y: 76 },
-    { x: 41, y: 70 },
-    { x: 65, y: 85 },
+    { x: 33, y: 75 },
+    { x: 41, y: 69 },
+    { x: 65, y: 84 },
     { x: 61.8, y: 48 },
     { x: 71, y: 43 },
     { x: 83, y: 35 },
   ];
 
-  mapWidth!: number;
-  mapHeight!: number;
-  topMargin!: string;
-  leftMargin!: string;
+  mapWidth: number = 0;
+  mapHeight: number = 0;
 
-  readonly aspectRatio: number = 16 / 9; // Ratio fixe du SVG
-
-  constructor() {}
+  constructor() { }
 
   ngOnInit() {
     this.updateMapDimensions();
@@ -47,30 +44,31 @@ export class MapComponent {
   }
 
   private updateMapDimensions() {
-    const container = document.querySelector('.map-container') as HTMLElement;
-    const containerWidth = container.offsetWidth;
-    const containerHeight = container.offsetHeight;
+    const mapElement = document.querySelector('.map') as HTMLElement;
+    const backgroundImage = new Image();
+    backgroundImage.src = '/assets/img/map.svg';
+    backgroundImage.onload = () => {
+      const imageRatio = backgroundImage.width / backgroundImage.height;
+      const viewportWidth = window.innerWidth;
+      const viewportHeight = window.innerHeight;
 
-    // Calculer dimensions idéales pour conserver l'aspect ratio
-    if (containerWidth / containerHeight > this.aspectRatio) {
-      // Trop large : ajuster à la hauteur
-      this.mapHeight = containerHeight;
-      this.mapWidth = containerHeight * this.aspectRatio;
-      this.topMargin = '0';
-      this.leftMargin = `${(containerWidth - this.mapWidth) / 2}px`;
-    } else {
-      // Trop haut : ajuster à la largeur
-      this.mapWidth = containerWidth;
-      this.mapHeight = containerWidth / this.aspectRatio;
-      this.topMargin = `${(containerHeight - this.mapHeight) / 2}px`;
-      this.leftMargin = '0';
-    }
+      if (viewportWidth / imageRatio <= viewportHeight) {
+        this.mapWidth = viewportWidth;
+        this.mapHeight = viewportWidth / imageRatio;
+      } else {
+        this.mapHeight = viewportHeight;
+        this.mapWidth = viewportHeight * imageRatio;
+      }
+
+      mapElement.style.width = `${this.mapWidth}px`;
+      mapElement.style.height = `${this.mapHeight}px`;
+    };
   }
 
   getPointStyle(point: { x: number, y: number }) {
     return {
       left: `${(point.x / 100) * this.mapWidth}px`,
-      top: `${(point.y / 100) * this.mapHeight}px`
+      top: `${((point.y / 100) * this.mapHeight)}px`
     };
   }
 }
