ca7071d1ccefceeb110da6212a65e14742015ae4
Aurélien Houdart <aure.houdart@gmail.com>
2024-12-06 02:13:22 +0100

    feat/like la maquette sans les audios

--- src/app/Pages/podcast/podcast.component.ts
+++ src/app/Pages/podcast/podcast.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from "../../Components/header/header.component";
 import { PodcastCardComponent } from '../../Components/podcast-card/podcast-card.component';
+import {Button} from 'primeng/button';
 
 @Component({
   selector: 'app-podcast',
@@ -9,7 +10,8 @@ import { PodcastCardComponent } from '../../Components/podcast-card/podcast-card
   imports: [
     CommonModule,
     HeaderComponent,
-    PodcastCardComponent
+    PodcastCardComponent,
+    Button
   ],
   templateUrl: './podcast.component.html',
   styleUrls: ['./podcast.component.scss']
@@ -30,14 +32,40 @@ export class PodcastComponent {
       title: 'Nom de Podcast 3',
       date: '05/12/2024',
       imageUrl: 'assets/img/podcast_exemple.png'
+    },
+    {
+      title: 'Nom de Podcast 4',
+      date: '05/12/2024',
+      imageUrl: 'assets/img/podcast_exemple.png'
+    },
+    {
+      title: 'Nom de Podcast 5',
+      date: '05/12/2024',
+      imageUrl: 'assets/img/podcast_exemple.png'
+    },
+    {
+      title: 'Nom de Podcast 6',
+      date: '05/12/2024',
+      imageUrl: 'assets/img/podcast_exemple.png'
     }
   ];
 
   currentPodcast: any = null;
-  //audio: HTMLAudioElement;
+  @ViewChild('audioPlayer') audioPlayer!: ElementRef;
+  @ViewChild('podcastCards') podcastCards!: ElementRef;
   isPlaying: boolean = false;
   currentTime: number = 0;
   interval: any;
+  scrollAmount = 200; // Ajustez cette valeur selon vos besoins
+  scrollLeft() {
+    if (this.podcastCards && this.podcastCards.nativeElement) {
+      this.podcastCards.nativeElement.scrollBy({ left: -this.scrollAmount, behavior: 'smooth' });
+    }
+  }
 
-
+  scrollRight() {
+    if (this.podcastCards && this.podcastCards.nativeElement) {
+      this.podcastCards.nativeElement.scrollBy({ left: this.scrollAmount, behavior: 'smooth' });
+    }
+  }
 }
